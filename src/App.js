f638e22dbe5fc403ee4cff8deeaebafa92c70ba2
Vitor Kepeler <vitorkepeler@gmail.com>
2021-02-07 17:45:02 -0300

    Pronto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components'
 
@@ -74,18 +74,16 @@ function App() {
   const [zetas, setarZetas] = useState([
 
   ]);
+  
 
   const calcularZeta = () => {
 
-    var n1 = 2;
-    var n2 = 2;
-    var n3 = 2;
-    var n4 = 2;
+    var n = 2;
     const input = document.getElementById('num').value;
     var resp = 0;
 
-    while (fibonacci(n1) <= input) {
-      resp = fibonacci(n1);
+    while (fibonacci(n) <= input) {
+      resp = fibonacci(n);
 
       if (resp == input) {
 
@@ -103,141 +101,75 @@ function App() {
         var continuar = true;
       }
 
-      n1++;
+      n++;
     }
     if (continuar == true) {
-      n1--;
+      n--;
 
-      var f1 = fibonacci(n1);
-      var f2;
-
-      while (fibonacci(n2) <= resp) {
-        resp = f1 + fibonacci(n2);
-
-        if (resp == input) {
-          f2 = fibonacci(n2);
-
-          setarZetas([
-            {
-              zeta: 1,
-              certo: false
-            },
-            {
-              zeta: 2,
-              certo: true
-            }
-
-          ]);
-
-        }
-        if (resp > input) {
-          n2--
-
-          f2 = fibonacci(n2);
-
-          while (fibonacci(n3) <= resp) {
-
-            resp = f1 + f2 + fibonacci(n3);
-
-            if (resp == input) {
-              var f3 = fibonacci(n3);
-
-
-              setarZetas([
-                {
-                  zeta: 1,
-                  certo: false
-                },
-                {
-                  zeta: 2,
-                  certo: false
-                },
-                {
-                  zeta: 3,
-                  certo: true
-                }
-
-              ]);
-
-              break;
-            }
-            if (resp > input) {
-              n3--
-
-              var f3 = fibonacci(n3);
-
-              while (fibonacci(n4) <= resp) {
-
-                resp = f1 + f2 + f3 + fibonacci(n4);
-
-                if (resp == input) {
-                  var f4 = fibonacci(n4);
-
-
-                  setarZetas([
-                    {
-                      zeta: 1,
-                      certo: false
-                    },
-                    {
-                      zeta: 2,
-                      certo: false
-                    },
-                    {
-                      zeta: 3,
-                      certo: false
-                    },
-                    {
-                      zeta: 4,
-                      certo: true
-                    }
-
-                  ]);
-
-                  break;
-                }
-                n4++;
+      var z = 2;
+      var resp = fibonacci(n);
+      n = 2;
+      while (resp != input) {
+        while (resp <= input) {
+          if (resp + fibonacci(n) == input) {
+            resp = resp + fibonacci(n);
+            setarZetas([
+              {
+                zeta: z,
+                certo: true
               }
+            ]);
+            z--;
+            break;
+          } else {
+            if (resp + fibonacci(n) > input){
+              n--;
+              resp = resp +fibonacci(n);
               break;
+            }else{
+              n++
             }
-            n3++;
           }
-          break;
         }
-        n2++;
+        if (resp == input) {
+          break;
+        } else {
+          n=2;
+          z++
+        }
+
       }
 
     }
+  }
+  return (
 
-}
-return (
-
-  <div className="App">
-    <GlobalStyle />
-    <H1>DESAFIO CENTRAL DO FRANQUEADO</H1>
-    <DivInput>
-      <H3>Insira o número</H3>
-      <input id="num" type="number" />
-      <Button onClick={() => calcularZeta()}>Calcular Zeta</Button>
-    </DivInput>
-    <DivResult>
-      <H3>Zetas:</H3>
-      {
-        zetas.map((val) => {
-          if (val.certo) {
-            return (
-              <h1> {val.zeta}</h1>
-            );
-          } else {
-            return (
-              <P> {val.zeta}</P>
-            )
-          }
-        })
-      }
-    </DivResult>
-  </div>
-);
+    <div className="App">
+      <GlobalStyle />
+      <H1>DESAFIO CENTRAL DO FRANQUEADO</H1>
+      <DivInput>
+        <H3>Insira o número</H3>
+        <input id="num" type="number" />
+        <Button onClick={() => calcularZeta()}>Calcular Zeta</Button>
+      </DivInput>
+      <DivResult>
+        <H3>Zetas:</H3>
+        {
+          zetas.map((val) => {
+            if (val.certo) {
+              return (
+                <h1> {val.zeta}</h1>
+              );
+            } else {
+              return (
+                <P> {val.zeta}</P>
+              )
+            }
+          })
+        }
+      </DivResult>
+    </div>
+  );
 }
 
 export default App;
